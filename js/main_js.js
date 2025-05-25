7fa3c31c08b42f10524792bc0cfd41d508888a02
shummiee <saCena@mcm.edu.ph>
2025-05-25 14:27:28 +0800

    second

--- js/main_js.js
+++ js/main_js.js
@@ -6,3 +6,24 @@ images[current].classList.remove('active');
     current = (current + 1) % images.length;
     images[current].classList.add('active');
 }, 1000);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const loginModal = document.getElementById("loginModal");
+    const closeBtn = document.getElementById("closeLogin");
+    const userIcon = document.querySelector('a[aria-label="User"]');
+
+    userIcon.addEventListener("click", function (e) {
+        e.preventDefault();
+        loginModal.style.display = "flex";
+    });
+
+    closeBtn.addEventListener("click", function () {
+        loginModal.style.display = "none";
+    });
+
+    window.addEventListener("click", function (e) {
+        if (e.target === loginModal) {
+            loginModal.style.display = "none";
+        }
+    });
+});
