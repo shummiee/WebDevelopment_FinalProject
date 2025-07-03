d8387debf4fa531786f94fcd79be1f5f64040637
shummiee <saCena@mcm.edu.ph>
2025-07-03 08:53:35 +0800

    unfinished checkout UI

--- js/cart.js
+++ js/cart.js
@@ -1,18 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const selectAllFoot = document.querySelector(".cart-footer .select-all");
     const selectAllHead = document.querySelector(".select-all");
     const itemCheckboxes = document.querySelectorAll(".cart-item input[type='checkbox']:not(.sold-out-cb)");
 
-    // When select-all in footer is toggled
     selectAllHead.addEventListener("change", function () {
       itemCheckboxes.forEach((checkbox) => {
         checkbox.checked = selectAllHead.checked;
-      });
-    });
-
-    selectAllFoot.addEventListener("change", function () {
-      itemCheckboxes.forEach((checkbox) => {
-        checkbox.checked = selectAllFoot.checked;
       });
     });
 
@@ -21,13 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
       checkbox.addEventListener("change", function () {
         const allChecked = Array.from(itemCheckboxes).every(cb => cb.checked);
         selectAllHead.checked = allChecked;
-      });
-    });
-
-    itemCheckboxes.forEach((checkbox) => {
-      checkbox.addEventListener("change", function () {
-        const allChecked = Array.from(itemCheckboxes).every(cb => cb.checked);
-        selectAllFoot.checked = allChecked;
       });
     });
   });
